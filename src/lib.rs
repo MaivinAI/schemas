ce2a7bf3a4e8e746e5ce75d04de82682b1a5794e
Sébastien Taylor <sebastien@au-zone.com>
2024-04-13 10:46:52 -0600

    Applied Rust formatting.

--- src/lib.rs
+++ src/lib.rs
@@ -9,14 +9,13 @@
 /// * [rcl_interface](https://github.com/ros2/rcl_interfaces): Common interface in RCL
 /// * [foxglove_api_msgs](https://github.com/foxglove/schemas/tree/main/ros_foxglove_msgs)
 /// * [edgefirst_api_msgs](https://github.com/MaivinAI/schemas): EdgeFirst ROS messages
-
 pub mod common_interfaces;
-pub mod rcl_interfaces;
-pub mod foxglove_msgs;
 pub mod edgefirst_msgs;
+pub mod foxglove_msgs;
+pub mod rcl_interfaces;
 pub mod service;
 
 pub use common_interfaces::*;
-pub use rcl_interfaces::*;
-pub use foxglove_msgs::*;
 pub use edgefirst_msgs::*;
+pub use foxglove_msgs::*;
+pub use rcl_interfaces::*;
