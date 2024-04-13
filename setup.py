94b3319420628cf704fe32bbc351ce787ee571a4
Sébastien Taylor <sebastien@au-zone.com>
2024-04-13 16:53:05 -0600

    Adding Python package long description using package README.md

--- setup.py
+++ setup.py
@@ -22,4 +22,6 @@ setup(
     url='https://maivin.edgefirst.ai',
     packages=[ 'edgefirst.schemas' ],
     install_requires=[ 'pycdr2' ],
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',
 )
