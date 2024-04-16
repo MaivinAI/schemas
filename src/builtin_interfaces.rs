a64a341ee0a075ff2353044a8574c15a08afb20d
Sébastien Taylor <sebastien@au-zone.com>
2024-04-16 14:03:48 -0600

    Added Time conversion traits to u64.

--- src/builtin_interfaces.rs
+++ src/builtin_interfaces.rs
@@ -1,5 +1,7 @@
 use serde_derive::{Deserialize, Serialize};
 
+const NSEC_IN_SEC: u64 = 1_000_000_000;
+
 #[derive(Serialize, Deserialize, PartialEq, Clone)]
 pub struct Time {
     pub sec: i32,
@@ -10,4 +12,27 @@ pub struct Time {
 pub struct Duration {
     pub sec: i32,
     pub nanosec: u32,
+}
+
+impl Time {
+    pub fn new(sec: i32, nanosec: u32) -> Self {
+        Time { sec, nanosec }
+    }
+
+    pub fn from_nanos(nanos: u64) -> Self {
+        Time {
+            sec: (nanos / NSEC_IN_SEC) as i32,
+            nanosec: (nanos % NSEC_IN_SEC) as u32,
+        }
+    }
+
+    pub fn to_nanos(&self) -> u64 {
+        self.sec as u64 * NSEC_IN_SEC + self.nanosec as u64
+    }
+}
+
+impl From<Time> for u64 {
+    fn from(time: Time) -> Self {
+        time.to_nanos()
+    }
 }
