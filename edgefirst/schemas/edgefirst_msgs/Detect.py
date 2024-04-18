4986580dd0f62b66153f35b7c99dc653dcb1ddd2
Matt Quigley <matt@au-zone.com>
2024-04-18 17:14:14 -0600

    corrected Detect to be a sequence of boxes

--- edgefirst/schemas/edgefirst_msgs/Detect.py
+++ edgefirst/schemas/edgefirst_msgs/Detect.py
@@ -4,9 +4,11 @@
 
 from dataclasses import dataclass
 from pycdr2 import IdlStruct
+from pycdr2.types import sequence
 from ..builtin_interfaces.Time import Time
 from ..std_msgs.Header import Header
 from .Box import Box
+from .. import default_field
 
 @dataclass
 class Detect(IdlStruct, typename='edgefirst_msgs/Detect'):
@@ -16,5 +18,5 @@ class Detect(IdlStruct, typename='edgefirst_msgs/Detect'):
     model_time: Time =      Time() # Inference duration for the model to process the image
     output_time: Time =     Time() # Timestamp when the message was sent out
 
-    boxes: Box =             Box() # Array of detected object bounding boxes
-                                   # Each box includes normalized center point, width, and height
+    boxes: sequence[Box] = default_field([]) # Array of detected object bounding boxes
+                                             # Each box includes normalized center point, width, and height
